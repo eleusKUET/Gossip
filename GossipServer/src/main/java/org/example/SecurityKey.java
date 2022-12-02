97f847b4ef3be27944ce10cf68c8164f18b47b29
Eleus Ahammad <60877162+eleusKUET@users.noreply.github.com>
2022-12-02 21:51:57 +0600

    Update SecurityKey.java

--- GossipServer/src/main/java/org/example/SecurityKey.java
+++ GossipServer/src/main/java/org/example/SecurityKey.java
@@ -1,7 +1,6 @@
 package org.example;
 
 public class SecurityKey {
-    String key = "mongodb+srv://eleusahammad:wsZ7uEvWfHtRPsB1@cluster0.otxy04r.mongodb.net/?retryWrites=true&w=majority";
     String key2 = "mongodb://localhost:27017";
     SecurityKey(){}
     String get(){return key2;}
